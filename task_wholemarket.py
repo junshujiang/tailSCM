565b3b9595d7b77e8cad82ff566fb4f4bd050def
648847278@qq.com <648847278@qq.com>
2025-03-05 17:09:17 +0300

    modified:   task_wholemarket.py separated script for long time task 	modified:   tutorial3_applicationDanube.ipynb 	modified:   tutorial4_applicationChinaDerivatives.ipynb debug the column order, now the result make very good sense

--- task_wholemarket.py
+++ task_wholemarket.py
@@ -1,36 +1,40 @@
 import pandas as pd
-import time
 from datetime import datetime
 from helper_util import *
 import seaborn as sns
-import pickle
-import os
 from helper_simulation import expand_data_df
 current_date = datetime.now().strftime("%Y%m%d")    
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
-
 if __name__=="__main__":
+
+    INTERVAL=60
+    pc_alpha=0.001
+    quantile=0.05
+
+
     max_id=get_max("exp_result")
     exp_str=""
-    log_path=f"exp_result/{str(max_id)}.{current_date}.applicationChinaDerivativesWholeMarket"
+    log_path=f"exp_result/{str(max_id)}.{current_date}.applicationChinaDerivatives_I{INTERVAL}"
 
     df_price_volume=pd.read_parquet("/home/jianj0c/project/STP/dataset/price_volume.parquet")
 
     logger=get_logger(log_path)
 
-    INTERVAL=600
-    pc_alpha=0.001
-    quantile=0.5
 
     logger.info(f"INTERVAL: {INTERVAL}")
     logger.info(f"pc_alpha: {pc_alpha}")
     logger.info(f"quantile: {quantile}")
 
 
-    if 0:
+    data_path=f"/home/jianj0c/project/STP/dataset/price_volume{INTERVAL}.parquet"
+    if os.path.exists(data_path):
+        df_in_one=pd.read_parquet(data_path)
+    else:
+
+
         price_series=df_price_volume[[x for x in df_price_volume.columns if x.endswith(".price")]]
         return_df=pd.concat([price_series.pct_change(periods=INTERVAL),df_price_volume[["info.segment_index"]]],axis=1).iloc[::INTERVAL,].copy()
         return_df.loc[~return_df["info.segment_index"].diff().eq(0).fillna(False),[x for x in return_df.columns if not x.startswith("info.")]] = 0
@@ -63,13 +67,12 @@ if __name__=="__main__":
         volume_percentage.loc[~volume_percentage["info.segment_index"].diff().eq(0).fillna(False),[x for x in volume_percentage.columns if not x.startswith("info.")]] = 0
 
         df_in_one=pd.concat([volume_percentage[[x for x in volume_percentage.columns if x.endswith(".volume")]],return_df[[x for x in return_df.columns if  x.endswith(".price")]]],axis=1)
-
-    else:
-        df_in_one=pd.read_parquet(f"/home/jianj0c/project/STP/dataset/price_volume{INTERVAL}.parquet")
-
+        df_in_one.to_parquet(data_path)
 
 
     df_in_one=df_in_one.reset_index(drop=True).copy()
+
+    df_in_one.fillna(0,inplace=True)
 
 
 
@@ -78,6 +81,9 @@ if __name__=="__main__":
     for category in categories:
         all_assets.extend(categories[category])
 
+
+
+    len(all_assets)
 
     sub_df=df_in_one[[x for x in df_in_one.columns if x.split(".")[0][:-2] in all_assets and x.endswith(".price")]]
 
@@ -89,13 +95,24 @@ if __name__=="__main__":
     data_df_bar_vol=pd.concat([data_df_bar,sub_df_vol],axis=1)
 
 
-    before_time=time.time()
-    logger.info(f"start time: {before_time}")
 
     resultsThisPaper,results_tail=method_this_paper(data_df_bar_vol,pc_alpha=pc_alpha,quantile=quantile,tau_max=1,both_tail_variable=sub_df.shape[1])
 
-
+    import pickle
     pickle.dump(resultsThisPaper,open(os.path.join(log_path,"whole_market_resultsThisPaper.pkl"),"wb"))
     pickle.dump(results_tail,open(os.path.join(log_path,"whole_market_results_detail.pkl"),"wb"))
-    logger.info(f"end time: {time.time()}")
-    logger.info(f"time cost: {time.time()-before_time}")
+    pickle.dump(all_assets,open(os.path.join(log_path,"all_assets.pkl"),"wb"))
+
+
+    var_names=np.array([f"${i}^{{u}}$" for i in all_assets]+[f"${i}^{{l}}$" for i in all_assets]+[f"${i}.vol$" for i in all_assets])
+
+
+
+    save_path=os.path.join(log_path,f"all_assets_olut_timeseries_graph.png")
+
+    draw_graph_timeseries(vmin_edges=0,vmax_edges=1,show_colorbar=False,save_path=save_path,**sort_name_and_edge_price_volume(resultsThisPaper,var_names),figsize=(50,50))
+
+    save_path=os.path.join(log_path,f"all_assets_olut_graph.png")
+
+    figsize=(50,50)
+    draw_graph(arrow_linewidth=3,arrowhead_size=5,node_size=0.02,label_fontsize=15,figsize=figsize,**sort_name_and_edge_price_volume(resultsThisPaper,var_names),save_path=save_path)
